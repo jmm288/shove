b3eb0000aca524f1d2684e1620176da8fafae6bb
jbarton <joshua.m.barton@gmail.com>
2020-01-07 23:34:02 -0500

    Setting template skeleton, and settings.

--- shove/run.sh
+++ shove/run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-/opt/venv/app/bin/gunicorn -b 0.0.0.0:5000 shove.wsgi
+/opt/venv/app/bin/gunicorn -b 0.0.0.0:5000 shove.wsgi:application \
+  --workers 3 \
+  --log-level=debug \
+  "$@"
--- shove/shove/settings.py
+++ shove/shove/settings.py
@@ -50,11 +50,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'shove.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.realpath(os.path.dirname(__file__)), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +65,7 @@ TEMPLATES = [
         },
     },
 ]
+print(TEMPLATES)
 
 WSGI_APPLICATION = 'shove.wsgi.application'
 
